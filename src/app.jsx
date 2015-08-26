4dedf1727343da7a868ef56bccfd3a90d2457769
Richard James <richard.james@mygravity.co>
2015-08-26 23:19:08 +0100

    Todo list app

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,76 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var FireBase = require('firebase');
+var Header = require('./header');
+var List = require('./list');
+var rootUrl = 'https://radiant-heat-2684.firebaseio.com/';
 
-var Hello = React.createClass({
+var App = React.createClass({
+  // shares methods of ReactFire
+  mixins: [ ReactFire ],
+  getInitialState: function(){
+     return{
+              items: {},
+              loaded: false
+          }
+ },
+  
+  
+  componentWillMount: function(){
+    this.fb = new FireBase(rootUrl + 'items/')
+    this.bindAsObject(this.fb, 'items'); 
+    this.fb.on('value',this.handleDataLoaded);
+    
+  
+  },
+  
   render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
+    return <div className="row panel panel-default">
+        <div className="col-md-8 col-md-offset-2">
+            <h2 className="text-center">
+            To-Do List
+            </h2>
+            <Header itemsStore={this.firebaseRefs.items} />
+            <hr/>
+            <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+                <List items={this.state.items} />
+                {this.deleteButton()}
+            </div>
+        </div>
+      </div>        
+  },
+deleteButton: function(){
+
+    if(!this.state.loaded){
+        return
+    }else{
+    
+        return <div className="text-center clear-complete">
+            <hr />
+        <button 
+            type="button"
+            onClick={this.onDeleteDoneClick}
+            className="btn btn-default">Clear Complete
+        </button>
+        </div>
+    }
+
+},
+onDeleteDoneClick: function(){
+
+    for(var key in this.state.items){
+        if(this.state.items[key].done === true){
+            this.fb.child(key).remove();
+        }
+    }
+    
+},
+handleDataLoaded: function(){
+
+    this.setState({loaded: true});
+
+}
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
---
+++ src/list-item.jsx
@@ -0,0 +1,101 @@
+var React = require('react');
+var FireBase = require('firebase');
+var rootUrl = 'https://radiant-heat-2684.firebaseio.com/';
+
+module.exports = React.createClass({
+    getInitialState: function(){
+    
+        return {
+         text: this.props.item.text,
+         done: this.props.item.done,
+         textChanged: false
+        }
+    },
+    
+    componentWillMount: function(){
+    
+        this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);    
+    
+    },
+
+    render: function(){
+    
+        return <div className="input-group">
+            <span className="input-group-addon">
+            <input type="checkbox" 
+                   checked={this.state.done}
+                   onChange={this.handleDoneChange}
+                   
+            
+            />
+            </span>
+            <input type="text"
+                   disabled={this.state.done}
+                   className="form-control"
+                   value={this.state.text}
+                   onChange={this.handleTextChange}/>
+            
+            <span className="input-group-btn">
+                {this.changesButtons()}
+                <button className="btn btn-default"
+                        onClick={this.handleDeleteClick}
+                >Delete</button>
+            </span>
+        </div>
+        
+    },
+    
+    changesButtons: function(){
+    
+        if(!this.state.textChanged){
+        
+            return null
+        
+        }else {
+        
+            return [<button 
+                    onClick={this.handleSaveClick}
+                    className="btn btn-default">Save</button>,
+                    <button
+                    onClick={this.handleUndoClick}
+                    className="btn btn-default">Undo</button>
+                   ]
+        
+        }
+    
+    },
+    handleSaveClick: function(){
+    
+        this.fb.update({text: this.state.text});
+        this.setState({textChanged: false});
+    
+    },
+    handleUndoClick: function(){
+    
+        this.setState({
+            text: this.props.item.text,
+            textChanged: false
+        });
+    },
+    handleDoneChange: function(event){
+        var update = {done: event.target.checked}
+        this.setState(update);
+        this.fb.update(update);
+    
+    },
+    handleDeleteClick: function(){
+    
+        this.fb.remove();
+        
+    },
+    
+    handleTextChange: function(event){
+        this.setState({
+        
+            text: event.target.value,
+            textChanged: true
+        });
+
+    }
+
+});
